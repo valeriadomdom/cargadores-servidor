0f3bd837e2ce9a8c9ccc3481c5ad86414aff8e86
valeriadomdom <nube@kemtecnia.com>
2026-05-19 10:08:33 +0200

    fix: puerto 8080

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.post('/login', async function(req, res) {
   res.json({ token, rol: usuario.rol });
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', function() {
   console.log('Servidor corriendo en el puerto ' + PORT);
 });
